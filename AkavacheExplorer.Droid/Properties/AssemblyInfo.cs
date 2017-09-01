6167ebaca04720685b6dea132fa369fb188054f1
Andrius Kirilovas <andriuskirilovas@gmail.com>
2017-09-01 15:54:31 +0300

    Added [assembly: LinkerSafe] attribute

--- AkavacheExplorer.Droid/Properties/AssemblyInfo.cs
+++ AkavacheExplorer.Droid/Properties/AssemblyInfo.cs
@@ -3,6 +3,7 @@ using System.Runtime.CompilerServices;
 
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
+using Android;
 
 [assembly: AssemblyTitle("AkavacheExplorer.Droid")]
 [assembly: AssemblyDescription("")]
@@ -24,3 +25,5 @@ using System.Runtime.CompilerServices;
 
 //[assembly: AssemblyDelaySign(false)]
 //[assembly: AssemblyKeyFile("")]
+
+[assembly: LinkerSafe]
--- AkavacheExplorer.iOS/Properties/AssemblyInfo.cs
+++ AkavacheExplorer.iOS/Properties/AssemblyInfo.cs
@@ -3,6 +3,7 @@ using System.Runtime.CompilerServices;
 
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
+using Foundation;
 
 [assembly: AssemblyTitle("AkavacheExplorer.iOS")]
 [assembly: AssemblyDescription("")]
@@ -24,3 +25,5 @@ using System.Runtime.CompilerServices;
 
 //[assembly: AssemblyDelaySign(false)]
 //[assembly: AssemblyKeyFile("")]
+
+[assembly: LinkerSafe]
